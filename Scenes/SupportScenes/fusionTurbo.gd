04a92d8d01169ad8eb25e6a8fa906b9f5bbf1456
henriqua <hondohondo@gmail.com>
2021-11-22 00:00:14 -0800

    messed up
    car is falling through terrain.  May need a fresh start.

--- Scenes/SupportScenes/fusionTurbo.gd
+++ Scenes/SupportScenes/fusionTurbo.gd
@@ -1,32 +1,20 @@
-extends KinematicBody2D
+extends RigidBody2D
 
+var wheels = []
+var speed = 600
+var max_speed = 50
 
-const GRAVITY = 700
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
-const UP = Vector2(0, -1) 
-
-var velocity = Vector2.ZERO
-
+func _ready():
+	wheels = get_tree().get_nodes_in_group("wheel")
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	
-	velocity.y += GRAVITY * delta	
-	
-	#if Input.is_action_pressed("ui_right"):
-		
-	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if Input.is_action_pressed("ui_right"):
+		for wheel in wheels:
+			if wheel.angular_velocity < max_speed:
+				wheel.apply_torque_impulse(speed * delta * 60)
 
-	
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	
-func _process(delta):
-	velocity = move_and_slide_with_snap(velocity, UP)
+	if Input.is_action_pressed("ui_left"):
+		for wheel in wheels:
+			if wheel.angular_velocity > -max_speed:
+				wheel.apply_torque_impulse(-speed * delta * 60)
 	
